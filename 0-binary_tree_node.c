ae56f2d92edcad8d1c1167dce0a438a04669ff4d
EvaMicich <6366@holbertonstudents.com>
2023-05-14 04:55:11 -0700

    corrected node to match binary tree, no tlinked list

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,23 +11,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	new_node = malloc(sizeof(new_node));
+	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = value;
-	new_node->parent = *head;
+	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (*head != NULL)
-	{
-		(*head)->prev = new_node;
-	}
-	*head = new_node;
-	return (*head);
+	return (new_node);
 }
