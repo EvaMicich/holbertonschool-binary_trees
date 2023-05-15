fc224472c8580a6dbb3cbe1c6cb4bcefc899d10b
EvaMicich <6366@holbertonstudents.com>
2023-05-14 21:39:21 -0700

    added code to find height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_postorder - goes through binary tree using post-order traversal
- * @tree: a pointer to the root node of the tree to traverse
+ * recursion_height - finds height + 1
+ * @tree: a pointer to the root node of the tree
  *
- * Return: nothing
+ * Return: success returns height of tree + 1, failure 0
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t recursion_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	left_height = binary_tree_height(tree->left);
-
-	right_height = binary_tree_height(tree->right);
+	left_height = recursion_height(tree->left);
+	right_height = recursion_height(tree->right);
 
 	if (left_height > right_height)
 	{
@@ -30,12 +30,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		right_height = right_height + 1;
 		return (right_height);
 	}
+}
 
+/**
+ * binary_tree_height - finds height of binary tree
+ * @tree: a pointer to the root node of the tree
+ *
+ * Return: success returns height of tree, failure 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0;
 
-	if (tree != NULL && func != NULL)
-	{
-		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func);
-		func(tree->n);
-	}
+	height = recursion_height(tree) - 1;
+	return (height);
 }
