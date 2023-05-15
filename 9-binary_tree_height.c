52dbb71a770dc60c2a52944501e9bd4c61f9bd1d
EvaMicich <6366@holbertonstudents.com>
2023-05-14 21:42:26 -0700

    fixed null edged case

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -42,6 +42,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height = 0;
 
+	if (tree == NULL)
+	{
+		return (0);
+	}
 	height = recursion_height(tree) - 1;
 	return (height);
 }
