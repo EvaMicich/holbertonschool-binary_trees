f2c48d527579f11fc997527441fa78c673fa9b6c
belindaHBTN <6372@holbertonstudents.com>
2023-05-15 07:27:27 -0700

    finished the full tree checker

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * is_only_child - check is the node is a leaf
- * @node: the check
+ * is_only_child - check if the node only has one child
+ * @node: the node that need to be checked
  *
- * Return: 1 for leaf, 0 for not a leaf
+ * Return: 1 for the node only has one child , 0 for
+ * the node has two children or has no child
  */
 
 int is_only_child(const binary_tree_t *node)
@@ -29,12 +30,12 @@ int is_only_child(const binary_tree_t *node)
 }
 
 /**
- * binary_tree_is_full - check if binary tree is full or not
+ * recursion_full - count if there is any node with only one child
  * @tree: a pointer to the root node of the tree
  *
  * Return: 1 for full tree, failure 0
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int recursion_full(const binary_tree_t *tree)
 {
 	size_t count = 0;
 	size_t left_count = 0;
@@ -48,11 +49,31 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (1);
 	}
-	left_count = binary_tree_is_full(tree->left);
-	right_count = binary_tree_is_full(tree->right);
 
+	left_count = recursion_full(tree->left);
+	right_count = recursion_full(tree->right);
 	count = left_count + right_count;
-	printf("count is %zu\n", count);
+
+	return (count);
+}
+
+/**
+ * binary_tree_is_full - check if binary tree is full or not
+ * @tree: a pointer to the root node of the tree
+ *
+ * Return: 1 for full tree, failure 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int count = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	count = recursion_full(tree);
+
 	if (count > 0)
 	{
 		return (0);
