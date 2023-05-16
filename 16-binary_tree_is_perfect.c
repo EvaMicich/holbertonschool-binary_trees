b361bc42a66e557df2a2b62a329815b6428952f8
EvaMicich <6366@holbertonstudents.com>
2023-05-15 18:27:56 -0700

    changed method, used leaf count and height relation

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * recursion_height - finds height + 1
+ * binary_tree_height - finds height of binary tree
  * @tree: a pointer to the root node of the tree
  *
- * Return: success returns height of tree + 1, failure 0
+ * Return: success returns height of tree, failure 0
  */
-
-size_t recursion_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
 	size_t right_height = 0;
@@ -16,8 +15,8 @@ size_t recursion_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	left_height = recursion_height(tree->left);
-	right_height = recursion_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 	{
@@ -29,85 +28,37 @@ size_t recursion_height(const binary_tree_t *tree)
 		right_height = right_height + 1;
 		return (right_height);
 	}
-}
 
-/**
- * binary_tree_height - finds height of binary tree
- * @tree: a pointer to the root node of the tree
- *
- * Return: success returns height of tree, failure 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	int height = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	height = recursion_height(tree);
-	return (height);
 }
 /**
- * binary_tree_balance - gives balance factor for binary tree
- * @tree: tree to find factor
+ * binary_tree_is_leaf - check is the node is a leaf
+ * @node: the check
  *
- * Return: on success, balance factor. NULL on failure
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int balance_factor;
-	int left_height;
-	int right_height;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	left_height =  binary_tree_height(tree->left);
-	right_height =	binary_tree_height(tree->right);
-	balance_factor = left_height - right_height;
-	return (balance_factor);
-}
-
-
-
-/**
- * is_only_child - check if the node only has one child
- * @node: the node that need to be checked
- *
- * Return: 1 for the node only has one child , 0 for
- * the node has two children or has no child
+ * Return: 1 for leaf, 0 for not a leaf
  */
 
-int is_only_child(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 	{
 		return (0);
 	}
 
-	if (node->left == NULL && node->right != NULL)
+	if (node->left == NULL && node->right == NULL)
 	{
 		return (1);
 	}
-	else if (node->left != NULL && node->right == NULL)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }
 
 /**
- * recursion_full - count if there is any node with only one child
+ * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: a pointer to the root node of the tree
  *
- * Return: 1 for full tree, failure 0
+ * Return: success returns number of leaves of tree, failure 0
  */
-int recursion_full(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t count = 0;
 	size_t left_count = 0;
@@ -117,49 +68,34 @@ int recursion_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (is_only_child(tree) == 1)
+	if (binary_tree_is_leaf(tree) == 1)
 	{
 		return (1);
 	}
+	left_count = binary_tree_leaves(tree->left);
+	right_count = binary_tree_leaves(tree->right);
 
-	left_count = recursion_full(tree->left);
-	right_count = recursion_full(tree->right);
 	count = left_count + right_count;
-
 	return (count);
 }
 
 /**
- * binary_tree_is_full - check if binary tree is full or not
- * @tree: a pointer to the root node of the tree
- *
- * Return: 1 for full tree, failure 0
+ * _pow_recursion - calculates x to the power y
+ * @x: the base number
+ * @y: th epowers number
+ * Return: the vulue of the power x to y, -1 is error
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+size_t _pow_recursion(size_t x, size_t y)
 {
-	int count = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	count = recursion_full(tree);
-
-	if (count > 0)
-	{
-		return (0);
-	}
-	else
-	{
+	if (y == 0)
 		return (1);
-	}
+
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+		return (-1);
 }
-
-
-
-
-
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -169,12 +105,18 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t leaves;
+	size_t height;
+	size_t two = 2;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	if ((binary_tree_balance(tree) == 0 || binary_tree_balance(tree) == 1 || binary_tree_balance(tree) == -1) && (binary_tree_is_full(tree) == 1))
+	leaves = binary_tree_leaves(tree);
+	height = binary_tree_height(tree) - 1;
+	printf("leaves [%zu], height[%zu]\n", leaves, height);
+	if (leaves == _pow_recursion(two, height))
 	{
 		return (1);
 	}
